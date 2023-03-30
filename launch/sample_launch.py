52c91c047a2385ce60c3b25a17f040863b24a31e
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-03-30 09:40:48 +0200

    [#42072] src: samples: sample_publisher_node.cpp: Add sample for the publisher node
    Adds a sample for the publisher node. The sample is a simple node that
    publishes:
    -- A string message with the current date and time
    -- A video message with a video stream from a webcam
    -- Log message every published string message

--- launch/sample_launch.py
+++ launch/sample_launch.py
@@ -17,4 +17,18 @@ def generate_launch_description():
         ],
         output='both',
     )
-    return launch.LaunchDescription([sample_gui_node])
+
+    sample_publish_node = ComposableNodeContainer(
+        name='sample_publish_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='gui_node',
+                plugin='gui_node::SamplePublisherComponent',
+                name='sample_publish_node')
+        ],
+        output='both',
+    )
+    return launch.LaunchDescription([sample_gui_node, sample_publish_node])
---
+++ src/samples/sample_publisher_node.cpp
@@ -0,0 +1,127 @@
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <string>
+#include <thread>
+
+#include "gui_node/gui_node.hpp"
+#include "gui_node/ros_data/ros_publisher_data.hpp"
+
+namespace gui_node
+{
+
+using RosStringPublisherData = RosPublisherData<std_msgs::msg::String, std::string>;
+using RosImagePublisherData = RosPublisherData<sensor_msgs::msg::Image, cv::Mat>;
+
+class SamplePublisherComponent
+{
+private:
+    std::shared_ptr<GuiNode> gui_node_ptr;       ///< Pointer to the GUI node
+    rclcpp::TimerBase::SharedPtr timer;          ///< Timer to publish the string
+    std::thread video_capture_thread;            ///< Thread to capture and publish video frames
+    cv::VideoCapture video_capture;              ///< Video capture object
+    std::atomic<bool> stop_video_capture_thread; ///< Flag to stop the video capture thread
+
+public:
+    SamplePublisherComponent(const rclcpp::NodeOptions &options) : stop_video_capture_thread(false)
+    {
+        gui_node_ptr = std::make_shared<GuiNode>(options, "gui_node");
+
+        // Create a string publisher
+        std::shared_ptr<RosStringPublisherData> string_publisher_data =
+            std::make_shared<RosStringPublisherData>(gui_node_ptr, "dateandtime",
+                                                     [](const std::string &date) -> std_msgs::msg::String
+                                                     {
+                                                         std_msgs::msg::String msg = std_msgs::msg::String();
+                                                         msg.data = date;
+                                                         return msg;
+                                                     });
+        gui_node_ptr->addRosData("string_publisher", string_publisher_data);
+
+        // Set the timer to publish the date and time
+        timer = gui_node_ptr->create_wall_timer(
+            std::chrono::milliseconds(1000),
+            [this]
+            {
+                time_t now = time(0);
+                std::string date = ctime(&now);
+                // Remove the newline character
+                date.pop_back();
+                std::shared_ptr<RosStringPublisherData> string_publisher_data =
+                    gui_node_ptr->getRosData("string_publisher")->as<RosStringPublisherData>();
+                RCLCPP_INFO(gui_node_ptr->get_logger(), "Publishing: %s", date.c_str());
+                string_publisher_data->publish(date);
+            });
+
+        // Create a video capture object
+        video_capture.open(0);
+        if (!video_capture.isOpened())
+        {
+            RCLCPP_ERROR(gui_node_ptr->get_logger(), "Could not open video capture device");
+            return;
+        }
+
+        // Create a video frames publisher
+        std::shared_ptr<RosImagePublisherData> video_publisher_data = std::make_shared<RosImagePublisherData>(
+            gui_node_ptr, "video",
+            [this](const cv::Mat &frame) -> sensor_msgs::msg::Image
+            {
+                sensor_msgs::msg::Image msg = sensor_msgs::msg::Image();
+                if (frame.empty())
+                {
+                    RCLCPP_ERROR(gui_node_ptr->get_logger(), "Frame is empty");
+                    return msg;
+                }
+                else if (frame.type() != CV_8UC4)
+                {
+                    RCLCPP_ERROR(gui_node_ptr->get_logger(), "Frame type is not CV_8UC4");
+                    return msg;
+                }
+                msg.header.stamp = rclcpp::Clock().now();
+                msg.header.frame_id = "video";
+                msg.encoding = "rgba8";
+                msg.height = frame.rows;
+                msg.width = frame.cols;
+                msg.step = static_cast<sensor_msgs::msg::Image::_step_type>(frame.step);
+                msg.data.assign(frame.datastart, frame.dataend);
+                return msg;
+            });
+
+        gui_node_ptr->addRosData("video_publisher", video_publisher_data);
+
+        // Start the thread to capture and publish video frames
+        video_capture_thread = std::thread(
+            [this]
+            {
+                cv::Mat frame;
+                while (rclcpp::ok() && !stop_video_capture_thread.load())
+                {
+                    video_capture >> frame;
+                    // Convert the frame to RGBA
+                    cv::cvtColor(frame, frame, cv::COLOR_BGR2RGBA);
+                    std::shared_ptr<RosImagePublisherData> video_publisher_data =
+                        gui_node_ptr->getRosData("video_publisher")->as<RosImagePublisherData>();
+                    video_publisher_data->publish(frame);
+                }
+            });
+    }
+
+    ~SamplePublisherComponent()
+    {
+        stop_video_capture_thread.store(true);
+        video_capture_thread.join();
+    }
+
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr get_node_base_interface()
+    {
+        return gui_node_ptr->get_node_base_interface();
+    }
+};
+
+} // namespace gui_node
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(gui_node::SamplePublisherComponent)
