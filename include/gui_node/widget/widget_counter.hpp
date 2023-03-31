22e5d9f56c216cb068f7d3a0ae0bc04b05079f16
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-03-31 16:44:50 +0200

    [#43008] widget: widget_counter: Add widget for '/counter' service client

---
+++ include/gui_node/widget/widget_counter.hpp
@@ -0,0 +1,35 @@
+#include <memory>
+#include <string>
+
+#include "gui_node/gui_node.hpp"
+#include "gui_node/ros_data/ros_client_data.hpp"
+#include "gui_node/widget/widget.hpp"
+
+namespace gui_node
+{
+
+class CounterWidget : public Widget
+{
+private:
+    int counter = 0; ///< Counter value
+
+public:
+    /**
+     * Constructor.
+     *
+     * @param gui_node Pointer to the GuiNode object that owns this widget.
+     * @param window_name Name of the widget's window.
+     * @param ros_data_name Name of the ROS data object that widget is associated with.
+     */
+    CounterWidget(std::shared_ptr<GuiNode> gui_node, const std::string &window_name, const std::string &ros_data_name)
+        : Widget(gui_node, window_name, ros_data_name)
+    {
+    }
+
+    /**
+     * Draw the widget.
+     */
+    void draw() override;
+};
+
+} // namespace gui_node
---
+++ src/widget/widget_counter.cpp
@@ -0,0 +1,51 @@
+#include <std_srvs/srv/trigger.hpp>
+
+#include "gui_node/ros_data/ros_client_data.hpp"
+#include "gui_node/widget/widget_counter.hpp"
+
+namespace gui_node
+{
+void CounterWidget::draw()
+{
+    // Get the data
+    using RosCounterClientData = RosServiceClientData<std_srvs::srv::Trigger, std_srvs::srv::Trigger::Response>;
+    std::shared_ptr<RosCounterClientData> ros_data =
+        this->gui_node->getRosData(ros_data_name)->as<RosCounterClientData>();
+    if (ros_data->hasDataChanged())
+    {
+        std_srvs::srv::Trigger::Response response = ros_data->getData();
+        if (response.success && response.message == "triggered")
+        {
+            counter++;
+        }
+    }
+
+    // Draw the widget
+    ImGui::Begin(window_name.c_str());
+    ImGui::SetWindowSize(ImVec2(200, 100), ImGuiCond_FirstUseEver);
+    ImGui::SetCursorPosX((ImGui::GetWindowWidth() - ImGui::CalcTextSize("Trigger").x) / 2);
+
+    // If service is not available, disable the button
+    if (ros_data->isServiceAvailable())
+    {
+        if (ImGui::Button("Trigger"))
+        {
+            // Make the request
+            std_srvs::srv::Trigger::Request::SharedPtr request = std::make_shared<std_srvs::srv::Trigger::Request>();
+            ros_data->sendRequest(request);
+        }
+    }
+    else
+    {
+        ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(0.0f, 0.6f, 0.6f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(0.0f, 0.7f, 0.7f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(0.0f, 0.8f, 0.8f));
+        ImGui::Button("Trigger");
+        ImGui::PopStyleColor(3);
+    }
+    std::string counter_str = "Counter: " + std::to_string(counter);
+    ImGui::SetCursorPosX((ImGui::GetWindowWidth() - ImGui::CalcTextSize(counter_str.c_str()).x) / 2);
+    ImGui::Text("%s", counter_str.c_str());
+    ImGui::End();
+}
+} // namespace gui_node
