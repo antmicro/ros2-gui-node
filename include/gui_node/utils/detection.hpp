fc45069eb7ea5305b4fca9ada03b5a787b076d00
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-07-04 15:29:38 +0200

    [#46369] include: utils: detection.hpp: Add bounding box structure
    Adds an object and bounding box structures.

---
+++ include/gui_node/utils/detection.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <imgui.h>
+
+namespace gui_node
+{
+
+/**
+ * Object structure.
+ */
+struct Object
+{
+    std::string label; ///< Label of the object
+    float score;       ///< Score of the object
+    ImColor color;     ///< Color of the object
+
+    /**
+     * Constructor.
+     *
+     * @param label Label of the object.
+     * @param score Score of the object.
+     * @param color Color of the object.
+     */
+    Object(std::string label, float score, ImColor color) : label(label), score(score), color(color) {}
+};
+
+/**
+ * Bounding box structure.
+ */
+struct BoundingBox
+{
+    float xmin;    ///< Minimum x coordinate cooefficient (0-1)
+    float ymin;    ///< Minimum y coordinate cooefficient (0-1)
+    float xmax;    ///< Maximum x coordinate cooefficient (0-1)
+    float ymax;    ///< Maximum y coordinate cooefficient (0-1)
+    Object object; ///< Bounding Box object
+
+    /**
+     * Constructor.
+     *
+     * @param xmin Minimum x coordinate cooefficient.
+     * @param ymin Minimum y coordinate cooefficient.
+     * @param xmax Maximum x coordinate cooefficient.
+     * @param ymax Maximum y coordinate cooefficient.
+     * @param color Color of the bounding box.
+     * @param label Label of the bounding box.
+     * @param score Score of the bounding box.
+     */
+    BoundingBox(float xmin, float ymin, float xmax, float ymax, std::string label, float score, ImColor color)
+        : xmin(xmin), ymin(ymin), xmax(xmax), ymax(ymax), object(Object(label, score, color))
+    {
+    }
+};
+
+} // namespace gui_node
