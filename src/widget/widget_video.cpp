59a73dff7f51f94cec7095b0eb35ed24bb74437e
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-07-04 12:25:45 +0200

    [#46373] src: widget: widget_video: Restrict window resize speed

--- src/widget/widget_video.cpp
+++ src/widget/widget_video.cpp
@@ -124,7 +124,12 @@ void VideoWidget::resizeCallback(ImGuiSizeCallbackData *data)
     float diffx = data->CurrentSize.x - data->DesiredSize.x;
     float diffy = data->CurrentSize.y - data->DesiredSize.y;
     int diff = diffx + diffy;
-    if (abs(diff) > 3)
+    if (abs(diff) > 50 && std::abs(diffx) > 0 && std::abs(diffy) > 0)
+    {
+        data->DesiredSize.x = data->CurrentSize.x - diff;
+        data->DesiredSize.y = ((data->DesiredSize.x - configs.offset.x) / configs.aspect_ratio) + configs.offset.y;
+    }
+    else if (std::abs(diffx) == 0 || std::abs(diffy) == 0)
     {
         data->DesiredSize.x = data->CurrentSize.x - diff;
         data->DesiredSize.y = ((data->DesiredSize.x - configs.offset.x) / configs.aspect_ratio) + configs.offset.y;
