ef0b31da718128c01d79025636b16bb10ddafcc7
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-07-24 22:51:20 +0200

    [#46795] examples: kenning-instance-segmentation: run-docker.sh: Added configuring camera path and Docker image
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- examples/kenning-instance-segmentation/run-docker.sh
+++ examples/kenning-instance-segmentation/run-docker.sh
@@ -2,12 +2,15 @@
 
 # This script runs the Docker container for ROS 2 and Kenning integration
 
+CAMERA_PATH=${CAMERA_PATH:-/dev/video0}
+DOCKER_IMAGE=${DOCKER_IMAGE:-ghcr.io/antmicro/ros2-gui-node:kenning-ros2-demo}
+
 docker run -it \
-    --device=/dev/video0:/dev/video0 \
+    --device=$CAMERA_PATH:$CAMERA_PATH \
     -v $(pwd):/data \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     --gpus='all,"capabilities=compute,utility,graphics,display"' \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-    ghcr.io/antmicro/ros2-gui-node:kenning-ros2-demo \
+    $DOCKER_IMAGE \
     /bin/bash
