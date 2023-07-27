d4be71d0876f85b5aa00733622bb090abd37fdc6
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-07-27 13:52:51 +0200

    [#46795] examples: kenning-instance-segmentation: Added script for building Docker image
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

---
+++ examples/kenning-instance-segmentation/build-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# This script builds the Docker container for ROS 2 and Kenning integration
+
+DOCKER_TAG=${DOCKER_TAG:-kenning-ros2-image}
+
+SCRIPTDIR=$(dirname $(realpath $0))
+
+pushd ${SCRIPTDIR}
+
+mkdir -p third-party/
+
+if [ ! -d "third-party/tvm" ]
+then
+    git clone --recursive --shallow-submodules https://github.com/apache/tvm.git --depth 1 --branch v0.12.0 third-party/tvm
+fi
+
+docker build . --tag ${DOCKER_TAG}
+
+popd
