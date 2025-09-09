b6973d7e06ea479077aa47b1edfe448d19ebae5d
Patryk Peroński <pperonski@internships.antmicro.com>
2025-09-09 07:25:56 +0100

    [#82173] examples: Added routines to run-docker.sh that checks for GPU if it is not present CPU only inference is used

--- examples/kenning-instance-segmentation/run-docker.sh
+++ examples/kenning-instance-segmentation/run-docker.sh
@@ -6,15 +6,47 @@
 
 # This script runs the Docker container for ROS 2 and Kenning integration
 
+if [[ $1 = "help" ]]; then
+    echo "Example of instance segmentation using Kenning and ROS 2"
+    echo 
+    echo "It utillizes NVIDIA GPU for faster inference but CPU can be used as well"
+    echo "In order to run example explicitly with CPU inference run:"
+    echo 
+    echo "  run-docker.sh cpu"
+    echo 
+    exit
+fi
+
 CAMERA_PATH=${CAMERA_PATH:-/dev/video0}
 DOCKER_IMAGE=${DOCKER_IMAGE:-ghcr.io/antmicro/ros2-gui-node:kenning-ros2-demo}
 
+GPU_PARAMS=--gpus='all,"capabilities=compute,utility,graphics,display"'
+
+USE_CPU=""
+
+if ! command -v nvidia-smi >/dev/null 2>&1
+then
+    echo "Nvidia GPU not found, using CPU"
+    GPU_PARAMS=""
+
+    USE_CPU="-e USE_CPU=1"
+fi
+
+if [[ $1 = "cpu" ]]; then
+    echo "Selected to run example on cpu"
+    GPU_PARAMS=""
+    
+    USE_CPU="-e USE_CPU=1"
+fi
+
 docker run -it \
+    --privileged \
     --device=$CAMERA_PATH:$CAMERA_PATH \
     -v $(pwd):/data \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-    --gpus='all,"capabilities=compute,utility,graphics,display"' \
+    $GPU_PARAMS \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    $USE_CPU \
     $DOCKER_IMAGE \
     /bin/bash
