13598c706b38d6a6459e3654e79bb383149676d2
Patryk Peroński <pperonski@internships.antmicro.com>
2026-01-16 13:05:52 +0100

    [#89235] environments: Unified Dockerfile, build-docker.sh and run-docker.sh scripts, switched base image from cuda to cudastack:standard.

--- environments/build-docker.sh
+++ environments/build-docker.sh
@@ -8,9 +8,20 @@
 
 DOCKER_TAG=${DOCKER_TAG:-ghcr.io/antmicro/ros2-gui-node:kenning-ros2-demo}
 
+BASE_IMAGE="nvidia/cuda:12.9.1-cudnn-devel-ubuntu24.04"
+BUILD_TVM="y"
+
 SCRIPTDIR=$(dirname "$(realpath "$0")")
 
 pushd "${SCRIPTDIR}" || exit
+if [[ $1 = "jetson" ]]; then
+    jetson-containers build cudastack:standard
+
+    BASE_IMAGE="$(autotag cudastack:standard)"
+    BUILD_TVM=""
+    ADDITIONAL_PACKAGES="kmod \
+    vulkan-validationlayers" 
+fi
 
 mkdir -p third-party/
 
@@ -20,6 +31,6 @@ then
     git clone --recursive https://github.com/ros-perception/vision_opencv --branch humble third-party/vision_opencv
 fi
 
-docker build . --tag "${DOCKER_TAG}"
+docker build . --tag "${DOCKER_TAG}" --build-arg BASE_IMAGE="$BASE_IMAGE" --build-arg BUILD_TVM="$BUILD_TVM"
 
 popd || exit
--- environments/run-docker.sh
+++ environments/run-docker.sh
@@ -6,42 +6,42 @@
 
 # This script runs the Docker container for ROS 2 and Kenning integration
 
-if [[ $1 = "help" ]]; then
-    echo "Example of instance segmentation using Kenning and ROS 2"
-    echo 
-    echo "It utillizes NVIDIA GPU for faster inference but CPU can be used as well"
-    echo "In order to run example explicitly with CPU inference run:"
-    echo 
-    echo "  run-docker.sh cpu"
-    echo 
-    exit
-fi
-
 CAMERA_PATH=${CAMERA_PATH:-/dev/video0}
 DOCKER_IMAGE=${DOCKER_IMAGE:-ghcr.io/antmicro/ros2-gui-node:kenning-ros2-demo}
 
 GPU_PARAMS=--gpus='all,"capabilities=compute,utility,graphics,display"'
 
-USE_CPU=""
+USE_PLATFORM=${USE_PLATFORM:""}
+
+INTERACTIVE_MODE="-it"
 
 if ! command -v nvidia-smi >/dev/null 2>&1
 then
     echo "Nvidia GPU not found, using CPU"
-    GPU_PARAMS=""
+    USE_PLATFORM="cpu"
 fi
 
-if [[ $1 = "cpu" ]]; then
+if [[ $USE_PLATFORM = "cpu" ]]; then
     echo "Selected to run example on cpu"
-    GPU_PARAMS=""    
+    GPU_PARAMS=""
+elif [[ $USE_PLATFORM = "jetson" ]]; then
+    echo "Selected Nvidia Jetson platform"
+    GPU_PARAMS="--runtime nvidia"
 fi
 
-docker run -it \
-    --privileged \
+if [ -n "$USE_NON_INTERACTIVE" ]; then
+    INTERACTIVE_MODE=""
+fi
+
+docker run --privileged \
+    $INTERACTIVE_MODE \
     --device=$CAMERA_PATH:$CAMERA_PATH \
     -v $(pwd):/data \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     $GPU_PARAMS \
+    -e KENNING_CACHE_DIR="/data/kenning_cache" \
+    -e USE_PLATFORM=$USE_PLATFORM \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     $DOCKER_IMAGE \
-    /bin/bash -c "$2"
+    $1
